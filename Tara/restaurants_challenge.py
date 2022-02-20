12d9bd8a0590f34ae070f109152b43948ba3078d
TaraMay21 <tmpuff21@gmail.com>
2022-02-19 22:50:06 -0700

    Tara

--- Tara/restaurants_challenge.py
+++ Tara/restaurants_challenge.py
@@ -14,6 +14,7 @@ Go through the dictionary and print out the following 3 pieces of information ab
 '''
 
 
+
 restaurant = {
     "name": "Four Barrel Coffee",
     "url": "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
@@ -35,6 +36,11 @@ print(restaurant)
 # TODO: Write code to print the latitude and longitude of Four Barrel Coffee.
 # TODO: Write code to print the complete address of the Four Barrel Coffee, formatted as a string - it should include the address, city, state and the zip code.
 # TODO: Write code to print the URL of the website of Four Barrel Coffee.
+print(f'latitude is {restaurant["latitude"]}')
+print(f'longitude is {restaurant["longitude"]}')
+print(f'complete address is {restaurant["address1"]}, {restaurant["city"]}, {restaurant["state"]}, {restaurant["zip_code"]}')
+print(f'website is {restaurant["url"]}')
+
 
 
 print()
@@ -49,45 +55,79 @@ print("Question 2")
 # TODO: Print each dictionary
 
 # The dictionary for each restaurant should look something like this
-
 '''
-restaurant_1  = {
-    "name": "Subway",
-    "address" : "116th & Broadway, NY 10016",
-    "favourite_dish" : "Chicken BLT Sandwich"
+'''
+
+restaurant_1 = {
+    "name": "Cucina Sorella",
+    "address" : "4055 Adams Ave, San Diego, CA 92116",
+    "favourite_dish" : "Gnocchetti Sardi"
+
+
 }
+
+restaurant_2 = {
+    "name": "The Grill",
+    "address" : "1092 E River RD, Pray, MT 59065",
+    "favourite_dish" : "Chef Tasting Menu"
+
+
+
+}
+
+restaurant_3 = {
+    "name": "Felix",
+    "address" : "1023 Abott Kinney Blv, Venice, CA 90291",
+    "favourite_dish" : "EVERYTHING!"
+
+
+}
+print(restaurant_1)
+print(restaurant_2)
+print(restaurant_3)
 '''
 
+
+
+
+
+'''
 print()
 
 print("Question 3")
-'''
-Imagine that any 1 of your most favourite restaurants stopped serving your favourite dish there.
-Remove the 'favourite_dish' key value pair from that restaurant's dictionary
+
+#Imagine that any 1 of your most favourite restaurants stopped serving your favourite dish there.
+# Remove the 'favourite_dish' key value pair from that restaurant's dictionary
 '''
 
 # TODO: Remove the 'favourite_dish' key-value pair from one of your 3 restaurants
 # TODO: Print the new dictionary. The new dictionary should only contain 'name' and 'address' for that restaurant
-
+'''
+restaurant_1.pop('favourite_dish')
+print(restaurant_1)
 print()
 
 print("Question 4")
-'''
-Imagine that another one of your most favourite restaurants modified its address.
-Update just this value in that restaurant's dictionary
-'''
+
+
+# Imagine that another one of your most favourite restaurants modified its address.
+# update just this value in that restaurant's dictionary
+
 
 # TODO: Update the address field of 1 restaurant
 # TODO: Print the new address of the restaurant by accessing that field of the restaurant's dictionary
 # TODO: Print the updated dictionary.
-
+restaurant_2["address"] = '103 S 9th st, MT 59047'
+print(restaurant_2)
 print()
 
 
 print("Question 5")
-'''
-Printing out all 3 of our restaurants every time is getting annoying. Let's clean up our code!
-'''
-
+#Printing out all 3 of our restaurants every time is getting annoying. Let's clean up our code!
 # TODO: Put your 3 restaurant dictionaries into a list called `restaurants`
 # TODO: Loop through your list and print out the name and address of each restaurant
+restaurants = [restaurant_1,restaurant_2,restaurant_3]
+print(restaurants)
+
+for restaurant in restaurants:
+    print(restaurant["name"],restaurant["address"])
